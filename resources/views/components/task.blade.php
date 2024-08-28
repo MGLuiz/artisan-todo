9e0c6798c570f040659ad32590f6d007a3615a0d
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2024-08-27 23:18:32 -0300

    add: View do formulário de criação das tasks

--- resources/views/components/task.blade.php
+++ resources/views/components/task.blade.php
@@ -1,19 +1,19 @@
 <div class="task">
     <div class="title">
         <input type="checkbox" name="" id="" 
-            @if ($data && $data['done']) checked @endif
+            @if ($data && $data['is_done']) checked @endif
         />
         <span class="task-title">{{$data['title'] ?? null}}</span>
     </div>
     <div class="category">
         <div class="sphere"></div>
-        <span class="category-label">{{$data['category_id'] ?? null}}</span>
+        <span class="category-label">{{ $data['category']->title ?? null }}</span>
     </div>
     <div class="actions" style="cursor: pointer">
-        <a href="{{route('task.edit')}}">
+        <a href={{ route('task.edit', ['id' => $data['id']]) }}>
             <img src="/assets/icons/icon-edit.png" alt="Click to edit task">
         </a>
-        <a href="{{route('task.delete')}}">
+        <a href={{route('task.delete', ['id' => $data['id']])}}>
             <img src="/assets/icons/icon-delete.png" alt="Click to exclude task">
         </a>
     </div>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,6 +6,34 @@
     </a>
     </x-slot>
 
-    Tela de criar uma Task
-    <a href={{route('home')}} style="font-variant: small-caps">Go to Home</a>
+    <section id="create-task-section">
+        <h2>Create Task</h2>
+        <form action="" method="">
+            <div class="input-area">
+                <label for="title">Task Title:</label>
+                <input type="text" name="title" placeholder="Enter the task title" required>
+            </div>
+
+            <div class="input-area">
+                <label for="due_date">Date:</label>
+                <input type="date" name="due_date" id="due_date" required>
+            </div>
+            
+            <div class="input-area">
+                <label for="category">Category:</label>
+                <select name="category" id="">
+                    <option selected disabled value="">Select a category</option>
+                    <option value="">Category 1</option>
+                    <option value="">Category 2</option>
+                    <option value="">Category 3</option>
+                </select>
+            </div>
+            
+            <div class="input-area">
+                <label for="description">Description:</label>
+                <textarea name="description" id="description" placeholder="Enter the task description"></textarea>
+            </div>
+        </form>
+
+    </section>
 </x-layout>
